dc0eca8d7e52b27273a878aaf5b7cb7f83496148
Kalpanab87 <kalpana7.ece@gmail.com>
2019-12-15 00:51:40 +0000

    ways of declaring and initializing variables

---
+++ src/learnProgramming/DeclaringAndIntializingVariables.java
@@ -0,0 +1,46 @@
+package learnProgramming;
+
+public class DeclaringAndIntializingVariables {
+    public static void main(String[] args) {
+
+        //Declaring and Initializing in 2 lines
+
+        int myNumber;//Declaration
+
+        //System.out.println("My Number is"+myNumber);
+        myNumber = 10; //initialization
+
+        System.out.println("My Number ="+myNumber);
+
+        //Declaring and Initializing in 1 line
+
+        double myDouble = 7.50;
+        int i1 = 2, i3 = 3;
+
+        System.out.println("My Double is ="+ myDouble);
+
+        //Declaring multiple variables in 1 one
+        float myFloat1,myFloat2,myFloat3;
+
+        float myFloat4; float myFloat5; //This is also allowed
+
+        double myDouble1, double myDouble2; //Doesn't compile;
+
+       /* double myDouble1 = 0.355;
+        double myDouble2 = 57687; //Doesn't compile;
+        System.out.println("myDouble1 is"+ myDouble1);
+        System.out.println("myDouble2 is" + myDouble2);*/
+
+        int int1, int2;
+
+        int abc3,abc4;
+
+        //int int = 10; // doesn't compile : don't use keywords
+
+
+        System.out.println("abc"+"\n abcd" + "\n abcde" +"\n abcdef");
+
+
+    }
+
+}
